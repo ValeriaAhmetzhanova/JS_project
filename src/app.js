bad3193c75d711177db71cc91c01948532e7a9e3
Valeria Ahmetzhanova <lerka-girl@mail.ru>
2019-02-13 12:48:55 +0300

    3rd assignment done

---
+++ src/app.js
@@ -0,0 +1,4 @@
+//Подключение модулей библиотеки react import React from 'react';
+import ReactDOM from 'react-dom';
+//Простой react код. Hello World
+ReactDOM.render(<h1>Hello, world!</h1>,document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,1 @@
+const path = require('path'); module.exports = {    entry: path.join(__dirname, 'src', 'app.js'),    output: {        path: path.join(__dirname, 'src', 'dist'),        filename: 'index.js'    },    module: {        rules: [{            test: /\.js$/,            loader: 'babel-loader',            exclude: '/node_modules/'        }]    } };
